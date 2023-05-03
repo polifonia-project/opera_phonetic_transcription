a6813678122e3eb1a23bb4541df34fa92c242fe3
Rocco Tripodi <rocco.trip@gmail.com>
2023-05-03 13:12:44 +0200

    update

--- parse.py
+++ parse.py
@@ -5,6 +5,10 @@ def split_arpa_phonemes(arpa):
     splitted_word = []
     print(arpa)
     while arpa != '':
+        if arpa.startswith('LH'):
+            arpa = arpa.replace('LH', 'LY', 1)
+        if arpa.startswith('GN'):
+            arpa = arpa.replace('GN', 'NY', 1)
         candidates = [aphon for aphon in arpabet_phonemes if arpa.startswith(aphon)]
         lengths = [len(c) for c in candidates]
         try:
@@ -22,41 +26,41 @@ def split_arpa_phonemes(arpa):
         arpa = arpa.replace(aphone,'',1)
     return ' '.join(splitted_word)
 
-
-phon = {}
-arpabet = {}
-transcription = {}
-with open('data/ariaVioletta_phon') as f:
-    lines = f.readlines()
-
 with open('data/arpabet.tsv') as f:
     reader = csv.reader(f, delimiter='\t')
     arpabet_phonemes = [r[0] for r in reader]
 
-for line in lines:
-    tokens = line.split()
-    if len(tokens)>0:
-        first = tokens[0]
-        if first.isnumeric():
-            if first not in phon:
-                phon[first] = tokens
-            else:
-                arpabet[first] = tokens
-for k, v in phon.items():
-    phonetic = arpabet[k]
-    for word, arpa in zip(v, phonetic):
-        if not word.isnumeric() and not arpa.isnumeric() and arpa not in ['!','?', '.', ',', ';', ':', '-']:
-            arpa = arpa.replace('1','').replace('_','')
-            arpa_ = split_arpa_phonemes(arpa)
-            print(word.split('/')[0], arpa)
-            if arpa_ == '----ERROR-----':
-                transcription[word.split('/')[0].lower()] = arpa + '-----> ERROR'
-            else:
-                transcription[word.split('/')[0].lower()] = arpa_
+for aria in ['trovatore', 'rigoletto', 'norma']:
+    phon = {}
+    arpabet = {}
+    transcription = {}
+    with open('data/' + aria +'_phon') as f:
+        lines = f.readlines()
 
-ordered = collections.OrderedDict(sorted(transcription.items()))
+    for line in lines:
+        tokens = line.split()
+        if len(tokens)>0:
+            first = tokens[0]
+            if first.isnumeric():
+                if first not in phon:
+                    phon[first] = tokens
+                else:
+                    arpabet[first] = tokens
+    for k, v in phon.items():
+        phonetic = arpabet[k]
+        for word, arpa in zip(v, phonetic):
+            if not word.isnumeric() and not arpa.isnumeric() and arpa not in ['!','?', '.', ',', ';', ':', '-']:
+                arpa = arpa.replace('1','').replace('_','')
+                arpa_ = split_arpa_phonemes(arpa)
+                print(word.split('/')[0], arpa)
+                if arpa_ == '----ERROR-----':
+                    transcription[word.split('/')[0].lower()] = arpa + '-----> ERROR'
+                else:
+                    transcription[word.split('/')[0].lower()] = arpa_
 
-with open('data/ariaVioletta_transcription.tsv', 'w') as f:
-    writer = csv.writer(f, delimiter='\t')
-    for k, v in ordered.items():
-        writer.writerow([k,v])
+    ordered = collections.OrderedDict(sorted(transcription.items()))
+
+    with open('data/' + aria + '_transcription.tsv', 'w') as f:
+        writer = csv.writer(f, delimiter='\t')
+        for k, v in ordered.items():
+            writer.writerow([k,v])
